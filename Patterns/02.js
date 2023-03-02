740d5f180ec2a27e7365b10c007c4b7ee0a69262
Abhishek R <55942013+abhi4898@users.noreply.github.com>
2023-03-02 20:50:34 +0530

    initial commit

---
+++ Patterns/02.js
@@ -0,0 +1,24 @@
+/*
+
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * *
+
+*/
+
+
+function leftAngleTringle(rows){
+    let str = ''
+
+    for(let i=0; i<rows; i++){
+        for(let j=0; j<=i; j++){
+            str += '* '
+        }
+        str += '\n'
+    }
+    return str
+}
+
+console.log(leftAngleTringle(5))
