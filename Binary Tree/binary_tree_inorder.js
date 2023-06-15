60ea62c7aa99cc1211ad56954762c4c505aebcbe
Abhishek R <55942013+abhi4898@users.noreply.github.com>
2023-06-15 20:22:14 +0530

    initial commit

--- Binary Tree/binary_tree_inorder.js
+++ Binary Tree/binary_tree_inorder.js
@@ -2,7 +2,7 @@ class TreeNode {
   constructor(val, left, right) {
     this.val = val === undefined ? 0 : val;
     this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : left;
+    this.right = right === undefined ? null : right;
   }
 }
 
